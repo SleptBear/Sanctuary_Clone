f1089eefe4efc7e81b87966695211a4e080dd6c2
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-01-17 08:25:09 -0800

    package json front end install

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+// import * as sessionActions from '../../store/session';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
+import OpenModalButton from '../OpenModalButton';
+import './Navigation.css';
+
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+//   const dispatch = useDispatch();
+
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//   };
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <li>
+        <ProfileButton user={sessionUser} />
+        {/* <button onClick={logout}>Log Out</button> */}
+      </li>
+    );
+  } else {
+    sessionLinks = (
+      <li>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
+        <OpenModalButton
+          buttonText="Sign Up"
+          modalComponent={<SignupFormModal />}
+        />
+        {/* <NavLink to="/signup">Sign Up</NavLink> */}
+      </li>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+      </li>
+      {isLoaded && sessionLinks}
+    </ul>
+  );
+}
+
+export default Navigation;
---
+++ frontend/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ModalProvider, Modal } from './context/Modal';
+
+import './index.css';
+
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+import configureStore from './store';
+import * as sessionActions from './store/session';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+function Root() {
+  return (
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
