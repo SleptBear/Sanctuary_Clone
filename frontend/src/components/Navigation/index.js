3343e6d03a458a2a6eef9db7a454a4c69d4f67c2
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-01-17 11:08:27 -0800

    navigation modals added

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,51 +2,21 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-// import * as sessionActions from '../../store/session';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
-import OpenModalButton from '../OpenModalButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
-//   const dispatch = useDispatch();
-
-//   const logout = (e) => {
-//     e.preventDefault();
-//     dispatch(sessionActions.logout());
-//   };
-
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-        {/* <button onClick={logout}>Log Out</button> */}
-      </li>
-    );
-  } else {
-    sessionLinks = (
-      <li>
-        <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
-        />
-        <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-        {/* <NavLink to="/signup">Sign Up</NavLink> */}
-      </li>
-    );
-  }
 
   return (
     <ul>
       <li>
         <NavLink exact to="/">Home</NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
