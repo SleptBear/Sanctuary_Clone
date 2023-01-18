2ecf6205a15d69e59536283d8d393f88881baced
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-01-18 15:52:53 -0800

    fetched data from one spot

--- frontend/src/components/SpotShow.js
+++ frontend/src/components/SpotShow.js
@@ -3,27 +3,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from 'react-router-dom';
 import { getSpot } from "../store/spots";
 import SpotsIndex from "./SpotsIndex";
+import SpotIndexItem from "./SpotsIndexItem";
 
 const SpotShow = () => {
     const dispatch = useDispatch();
     let { spotId } = useParams();
-    // spotId = +spotId
     console.log(typeof spotId)
 
     // const [id, setId] = useState()
 
 
-    const spotsObj = useSelector(state => state.spot.spot)
-    console.log(spotsObj)
+    const spot= useSelector(state => state.spot.spot)
+    console.log("spots Obj", spot)
 
-    const spots = Object.values(spotsObj)
+    // const spot = Object.values(spotsObj)
     // const spot = useSelector(state => state.spot[+spotId])
     // const spots = Object.values(spot)
-    console.log('object.values', spots)
+    // console.log('object.values', spot)
 
     useEffect(() => {
         dispatch(getSpot(spotId))
-    }, [])
+    }, [dispatch])
+
+
 
 
 
@@ -32,11 +34,19 @@ const SpotShow = () => {
     // };
 
     return (
-        <h2>OneSpotShow</h2>
+        <section>
+        ID: {spot.id}
+        <br/>
+        Location: {spot.country}
+        <br/>
+        Owner: {spot.Owner.firstName}
+        <br/>
+        {/* <button onClick={}> */}
 
-
-
-
+        {/* </button> */}
+        <br/>
+        <Link to="/">Back Home</Link>
+      </section>
     )
 }
 
