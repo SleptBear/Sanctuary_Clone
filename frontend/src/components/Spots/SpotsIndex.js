09c831432b8561d9d111c70035a2fb4f0704558d
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-02-08 13:50:54 -0800

    looked over home page render

--- frontend/src/components/Spots/SpotsIndex.js
+++ frontend/src/components/Spots/SpotsIndex.js
@@ -12,23 +12,22 @@ import SpotCard from "../Card/SpotCard";
 
 const SpotsIndex = () => {
     const dispatch = useDispatch();
+    const spotsObj = useSelector(state => state.spot.spots)
+    // const oneSpot = useSelector(state => state.spot.spot)
     const [currentSpots, setCurrentSpots] = useState({})
     const [currentSpot, setCurrentSpot] = useState({})
-    const spotsObj = useSelector(state => state.spot.spots)
-    const oneSpot = useSelector(state => state.spot.spot)
     const spots = Object.values(spotsObj)
-    // const spotsArray = spots[0]
-    // const data = spotsArray.Spots
-    // console.log("LOOK", spotsObj)
-    // console.log("HERE", spots)
-    // console.log("NOW")
-    // console.log("data", data)
+
+    // console.log("Selector Spots", spotsObj)
+    console.log("Array Spots", spots)
+    // console.log("Selector Spot", oneSpot)
+
 
     useEffect(() => {
         dispatch(getSpots());
-        setCurrentSpots(spotsObj)
-        console.log("STATE VARIABLE", currentSpots)
-    }, [setCurrentSpots, useHistory])
+        // setCurrentSpots(spotsObj)
+        // console.log("STATE VARIABLE", currentSpots)
+    }, [currentSpots])
 
     if(!spots[0]) return null
 
