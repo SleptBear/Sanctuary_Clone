9fb37df38f029f48848311bde1ef7e7b010936bc
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-08-31 15:42:00 -0700

    logo and user menu properly aligned with details content

--- frontend/src/components/Icons/logo.js
+++ frontend/src/components/Icons/logo.js
@@ -1,6 +1,6 @@
 const Logo = () => {
     return (
-      <div style={{fontSize: "30px", display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '150px', color: 'rgb(227, 38, 69)'}}>
+      <div style={{fontSize: "30px", display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '160px', color: 'rgb(227, 38, 69)'}}>
         {/* <i className="fa-solid fa-tent"></i> */}
         <i className="fa-solid fa-campground"></i>
         <div style={{color: 'red'}}>Sanctuary</div>
